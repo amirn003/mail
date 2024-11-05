7ad6efbe4b13b9a486b56289eb6369a627854c1f
amirn003 <amir.naar@gmail.com>
2024-11-05 07:40:42 +0100

    request the email for a particular mailbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -42,6 +42,14 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+  // Get emails from mailbox
+  const email = get_emails(mailbox);
+  console.log(`Email: ${email}`)
+
+  // Display emails
+  display_email(email)
+
+
 }
 
 
@@ -77,6 +85,27 @@ function get_email(email_id) {
 
 // Display email when load_mailbox is called
 function display_email(email) {
+  const emailDiv = document.createElement('div');
+  emailDiv.innerHTML = email;
+  document.querySelector('#emails-view').appendChild(emailDiv);
+}
+
+
+// Request the email from a particular mailbox by making a GET request to /emails/<mailbox>
+function get_emails(mailbox) {
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => (event) =>{
+    event.preventDefault();
+    // Print emails
+    console.log(emails);
+    alert(emails)
+
+  });
+}
+
+// Display email when load_mailbox is called
+function display_email(email){
   const emailDiv = document.createElement('div');
   emailDiv.innerHTML = email;
   document.querySelector('#emails-view').appendChild(emailDiv);
